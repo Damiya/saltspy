6e20031476de696923a5c6145c2c412b1e63f62c
Kate von Roeder <katevonroeder@gmail.com>
2013-09-19 14:17:49 -0700

    Bunny bunny bunny

--- lib/saltspy.rb
+++ lib/saltspy.rb
@@ -1,14 +1,20 @@
-require "saltspy/version"
-
 require 'SocketIO'
-
 module Saltspy
   class Saltspy
     def launch
-      client = SocketIO.connect("http://www-cdn-twitch.saltybet.com:8000") do
+      client = SocketIO.connect('http://www-cdn-twitch.saltybet.com:8000') do
         before_start do
-          on_message { |message| puts "incoming message: #{message}" }
-          on_event('news') { |data| puts data.first } # data is an array fo things.
+          #How do I lift this shit out into my class scope? God only knows
+
+          require 'bunny'
+          conn = Bunny.new
+          conn.start
+          @ch = conn.create_channel
+          @exchange = @ch.default_exchange
+
+          on_message do |data|
+            @exchange.publish('WEBSOCKET_UPDATED', :routing_key => 'com.itsdamiya.tradecaravan.update_stats')
+          end
         end
       end
     end
--- saltspy.gemspec
+++ saltspy.gemspec
@@ -1,11 +1,10 @@
 # coding: utf-8
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'saltspy/version'
 
 Gem::Specification.new do |spec|
   spec.name          = "saltspy"
-  spec.version       = Saltspy::VERSION
+  spec.version       = '0.0.1'
   spec.authors       = ["Kate von Roeder"]
   spec.email         = ["katevonroeder@gmail.com"]
   spec.description   = %q{Spy on the saltybets}
